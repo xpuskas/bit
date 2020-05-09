0cf1157c4670f7024852cd32be0147048de7de24
David First <davidnet@gmail.com>
2020-05-08 20:31:02 -0400

    set process.setMaxListeners to zero to avoid warnings about the listeners

--- extensions/cli/app.js
+++ extensions/cli/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+process.setMaxListeners(0);
 var _interopRequireWildcard = require('@babel/runtime/helpers/interopRequireWildcard');
 
 var _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault');
